8f01f8bb8020e62e276ecdf4c145964d136b2d25
tenyears102 <tenyears102@foxmail.com>
2017-11-06 12:06:17 +0800

    * 修改公共响应返回日志记录方式

--- kong/lib/response.lua
+++ kong/lib/response.lua
@@ -3,7 +3,7 @@ local json = require 'json'
 local log = require 'kong.lib.log'
 
 local function response(data)
-  if data.code ~= 0 then
+  if data.code ~= 0 and data.code ~= 1 then
     log.err("code:"..data.code..",".."message:"..data.msg)
   end
   ngx.say(json.encode(data))
