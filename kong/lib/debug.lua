1c49160f47710f9215b1e3c220908f29ca75d196
tenyears102 <tenyears102@foxmail.com>
2017-11-12 16:21:57 +0800

    * 增加debug

--- kong/lib/debug.lua
+++ kong/lib/debug.lua
@@ -1,73 +1,107 @@
 local debug = {}
 
 function debug.dump(obj)
-    local getIndent, quoteStr, wrapKey, wrapVal, isArray, dumpObj
-    getIndent = function(level)
-        return string.rep("\t", level)
-    end
-    quoteStr = function(str)
-        str = string.gsub(str, "[%c\\\"]", {
-            ["\t"] = "\\t",
-            ["\r"] = "\\r",
-            ["\n"] = "\\n",
-            ["\""] = "\\\"",
-            ["\\"] = "\\\\",
-        })
-        return '"' .. str .. '"'
-    end
-    wrapKey = function(val)
-        if type(val) == "number" then
-            return "[" .. val .. "]"
-        elseif type(val) == "string" then
-            return "[" .. quoteStr(val) .. "]"
-        else
-            return "[" .. tostring(val) .. "]"
-        end
-    end
-    wrapVal = function(val, level)
-        if type(val) == "table" then
-            return dumpObj(val, level)
-        elseif type(val) == "number" then
-            return val
-        elseif type(val) == "string" then
-            return quoteStr(val)
-        else
-            return tostring(val)
-        end
-    end
-    local isArray = function(arr)
-        local count = 0 
-        for k, v in pairs(arr) do
-            count = count + 1 
-        end 
-        for i = 1, count do
-            if arr[i] == nil then
-                return false
-            end 
-        end 
-        return true, count
-    end
-    dumpObj = function(obj, level)
-        if type(obj) ~= "table" then
-            return wrapVal(obj)
-        end
-        level = level + 1
-        local tokens = {}
-        tokens[#tokens + 1] = "{"
-        local ret, count = isArray(obj)
-        if ret then
-            for i = 1, count do
-                tokens[#tokens + 1] = getIndent(level) .. wrapVal(obj[i], level) .. ","
-            end
-        else
-            for k, v in pairs(obj) do
-                tokens[#tokens + 1] = getIndent(level) .. wrapKey(k) .. " = " .. wrapVal(v, level) .. ","
-            end
-        end
-        tokens[#tokens + 1] = getIndent(level - 1) .. "}"
-        return table.concat(tokens, "\n")
-    end
-    return dumpObj(obj, 0)
+	local getIndent, quoteStr, wrapKey, wrapVal, isArray, dumpObj
+	getIndent = function(level)
+		return string.rep("\t", level)
+	end
+	quoteStr = function(str)
+		str = string.gsub(str, "[%c\\\"]", {
+			["\t"] = "\\t",
+			["\r"] = "\\r",
+			["\n"] = "\\n",
+			["\""] = "\\\"",
+			["\\"] = "\\\\",
+		})
+		return '"' .. str .. '"'
+	end
+	wrapKey = function(val)
+		if type(val) == "number" then
+			return "[" .. val .. "]"
+		elseif type(val) == "string" then
+			return "[" .. quoteStr(val) .. "]"
+		else
+			return "[" .. tostring(val) .. "]"
+		end
+	end
+	wrapVal = function(val, level)
+		if type(val) == "table" then
+			return dumpObj(val, level)
+		elseif type(val) == "number" then
+			return val
+		elseif type(val) == "string" then
+			return quoteStr(val)
+		else
+			return tostring(val)
+		end
+	end
+	local isArray = function(arr)
+		local count = 0 
+		for k, v in pairs(arr) do
+			count = count + 1 
+		end 
+		for i = 1, count do
+			if arr[i] == nil then
+				return false
+			end 
+		end 
+		return true, count
+	end
+	dumpObj = function(obj, level)
+		if type(obj) ~= "table" then
+			return wrapVal(obj)
+		end
+		level = level + 1
+		local tokens = {}
+		tokens[#tokens + 1] = "{"
+		local ret, count = isArray(obj)
+		if ret then
+			for i = 1, count do
+				tokens[#tokens + 1] = getIndent(level) .. wrapVal(obj[i], level) .. ","
+			end
+		else
+			for k, v in pairs(obj) do
+				tokens[#tokens + 1] = getIndent(level) .. wrapKey(k) .. " = " .. wrapVal(v, level) .. ","
+			end
+		end
+		tokens[#tokens + 1] = getIndent(level - 1) .. "}"
+		return table.concat(tokens, "\n")
+	end
+	return dumpObj(obj, 0)
+end
+
+function debug.print_t(t)  
+ 	local print_t_cache = {}
+ 	local function sub_print_t(t,indent)
+ 		if (print_t_cache[tostring(t)]) then
+ 			print(indent.."*"..tostring(t))
+ 		else
+ 			print_t_cache[tostring(t)] = true
+ 			if (type(t) == "table") then
+ 				for pos,val in pairs(t) do
+ 					if (type(val) == "table") then
+ 						print(indent.."["..pos.."] => "..tostring(t).." {")
+ 						sub_print_t(val,indent..string.rep(" ",string.len(pos) + 8))
+ 						print(indent..string.rep(" ",string.len(pos) + 6).."}")
+ 					elseif (type(val) == "string") then
+ 						print(indent.."["..pos..'] => "'..val..'"')
+ 					else
+ 						print(indent.."["..pos.."] => "..tostring(val))
+ 					end
+ 				end
+ 			else
+ 				print(indent..tostring(t))
+ 			end
+ 		end
+ 	end
+ 	if (type(t) == "table") then
+ 		print(tostring(t).." {")
+ 		sub_print_t(t,"  ")
+ 		print("}")
+ 	else
+ 		sub_print_t(t,"  ")
+ 	end
+ 	print()
 end
 
 return debug
