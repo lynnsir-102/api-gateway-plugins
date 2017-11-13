c7a912d720f62554abb906ab5e86dcc6c0a8eba0
tenyears102 <tenyears102@foxmail.com>
2017-11-14 01:06:16 +0800

    * 加密插件解析multipart

--- kong/plugins/api-defender/access.lua
+++ kong/plugins/api-defender/access.lua
@@ -9,83 +9,99 @@ local redis_index = require 'kong.lib.redis_index'
 local special = require 'kong.plugins.api-defender.special'
 local exception = require 'kong.plugins.api-defender.exception'
 
+local ngxReq = ngx.req
+
 local real_url = nil
 local server_security = nil
 
+local function init()
+    ngxReq.read_body()
+end
+
 local function postern(key, secret, args)
-	if next(args) == nil then
-		return false
-	end
-	for k,v in pairs(args) do
-		if k == key and args[k] == secret then
-			return true
-		end
-	end 
-	return false
+    if next(args) == nil then
+        return false
+    end
+    for k, v in pairs(args) do
+        if k == key and args[k] == secret then
+            return true
+        end
+    end 
+    return false
 end
 
 local function generate_security(args, first_key, second_key, salt)
-	if args and args[first_key] and args[second_key] then
-		return ngx.md5(args[first_key] .. args[second_key] .. salt)
-	else
-		resData(exception.DEFENDER_PARAMS_LACKING)
-	end
+    if args and args[first_key] and args[second_key] then
+        return ngx.md5(args[first_key] .. args[second_key] .. salt)
+    else
+        resData(exception.DEFENDER_PARAMS_LACKING)
+    end
 end
 
 local function check_special(uri, special)
-	real_url = string.lower(uri) 
-	if utils.table_contains(special, real_url) then
-		return true
-	end
-		return false
+    real_url = string.lower(uri) 
+    if utils.table_contains(special, real_url) then
+        return true
+    end
+    return false
 end
 
 local function check_cache()
-	local connections = require 'kong.lib.connections'
-	local red = connections.redis_conn(redis_index.API_DEFENDER)
-	local req_time_prefix = "security:md5:"
-	local req_time_key = req_time_prefix .. ngx.md5(real_url .. server_security)
-	local req_time_val = red:get(req_time_key)
-	if '1' == req_time_val then
-		resData(exception.DEFENDER_MORE)
-	else
-		local ok, err = red:set(req_time_key, '1')
-		if not ok then
-			log.err("Redis err:" .. err)
-		end
-		red:expire(req_time_key, 3600 * 10)
-	end
+    local connections = require 'kong.lib.connections'
+    local red = connections.redis_conn(redis_index.API_DEFENDER)
+    local req_time_prefix = "security:md5:"
+    local req_time_key = req_time_prefix .. ngx.md5(real_url .. server_security)
+    local req_time_val = red:get(req_time_key)
+    if '1' == req_time_val then
+        resData(exception.DEFENDER_MORE)
+    else
+        local ok, err = red:set(req_time_key, '1')
+        if not ok then
+            log.err("Redis set err:" .. err)
+        end
+        red:expire(req_time_key, 3600 * 10)
+    end
 end
 
 local function check_security(args, first_key, second_key, salt, security)
-	server_security = generate_security(args, first_key, second_key, salt)
-	local client_security = args[security]
-	if server_security ~= client_security then
-		resData(exception.DEFENDER_PARAMS_FAIL)
-	end
-	check_cache()
-	return true
+    server_security = generate_security(args, first_key, second_key, salt)
+    local client_security = args[security]
+    if server_security ~= client_security then
+        resData(exception.DEFENDER_PARAMS_FAIL)
+    end
+    check_cache()
+    return true
 end
 
 function access.execute(config)
-	local params = params()
-	local args, body = params.args, params.body
-	local uri = ngx.var.uri
-	local all_args = utils.table_merge(args,body)
-
-	local salt, security = config.salt_value, config.security_key_name
-	local first_key, second_key = config.first_key_name, config.second_key_name
-	local postern_key, postern_secret = config.postern_key_name, config.postern_secret_value
-
-	if true == postern(postern_key, postern_secret, all_args) then
-		return
-	end
-	if true == check_special(uri, special) then
-		return
-	end
-	if true == check_security(all_args, first_key, second_key, salt, security) then
-		return
-	end
+    local all_args = {}
+    local headers = ngxReq.get_headers()
+    
+    if "POST" == ngxReq.get_method() and string.find(headers['Content-Type'], 'multipart/form%-data') then
+        init()
+        local body_data = ngx.req.get_body_data()
+        local multipart = require("kong.lib.multipart")
+        local multipart_data = multipart(body_data, headers["content-type"])
+        all_args = multipart_data:get_all()
+    else
+        local params = params()
+        all_args = utils.table_merge(params.args, params.body)
+    end
+    local uri = ngx.var.uri
+    
+    local salt, security = config.salt_value, config.security_key_name
+    local first_key, second_key = config.first_key_name, config.second_key_name
+    local postern_key, postern_secret = config.postern_key_name, config.postern_secret_value
+    
+    if true == postern(postern_key, postern_secret, all_args) then
+        return
+    end
+    if true == check_special(uri, special) then
+        return
+    end
+    if true == check_security(all_args, first_key, second_key, salt, security) then
+        return
+    end
 end
 
 return access
